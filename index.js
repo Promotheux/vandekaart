8f3eef14a0176c5aeb4bb2ef6fbba445507ec5ed
Niels van der Schaaf <niels@vanderschaaf.tech>
2023-10-13 03:16:59 +0200

    Added a sqlite db instead of json, also remove is added

--- index.js
+++ index.js
@@ -3,42 +3,83 @@ const app = express();
 const path = require('path');
 const bodyParser = require('body-parser');
 const fs = require('fs');
+const sqlite3 = require('sqlite3').verbose();
+
+const db = new sqlite3.Database('locations.db');
+// Define a table structure (if it doesn't exist)
+db.run(`
+  CREATE TABLE IF NOT EXISTS locations (
+    id INTEGER PRIMARY KEY,
+    Type TEXT,
+    X REAL,
+    Y REAL,
+    Count INTEGER
+  )
+`);
 
 // Serve static files from the 'public' directory
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(bodyParser.json());
 
+
+
 // Handle POST request to add a location
 app.post('/add-location', (req, res) => {
     const newLocation = req.body;
 
-    // Load the existing locations
-    let locations = [];
-    try {
-        locations = JSON.parse(fs.readFileSync('locations.json'));
-    } catch (err) {
-        console.error('Error reading locations.json:', err);
-    }
-
-    // Add the new location to the array
-    locations.push(newLocation);
-
-    // Write the updated locations array back to locations.json
-    fs.writeFileSync('locations.json', JSON.stringify(locations, null, 2));
-
-    res.send('Location added successfully.');
+     // Insert the new location data into the SQLite database
+     const query = `
+     INSERT INTO locations (Type, X, Y, Count)
+     VALUES (?, ?, ?, ?)
+     `;
+ 
+    db.run(query, [newLocation.Type, newLocation.X, newLocation.Y, newLocation.Count], (err) => {
+        if (err) {
+            console.error('Error inserting data into SQLite:', err);
+            res.status(500).send('Error adding location to the database');
+        } else {
+            res.send('Location added successfully.');
+        }
+    });
 });
 
 app.get('/get-locations', (req, res) => {
-    let locations = [];
+    // Query the SQLite database to retrieve locations
+    const query = 'SELECT * FROM locations';
 
-    try {
-        locations = JSON.parse(fs.readFileSync('locations.json'));
-    } catch (err) {
-        console.error('Error reading locations.json:', err);
+    db.all(query, (err, locations) => {
+        if (err) {
+            console.error('Error querying SQLite database:', err);
+            res.status(500).json({ error: 'Error fetching locations' });
+        } else {
+            res.json(locations);
+        }
+    });
+});
+
+app.delete('/remove-location/:id', (req, res) => {
+    const locationId = req.params.id;
+    // Check if the ID is valid (positive integer)
+    if (!(/^\d+$/.test(locationId))) {
+        res.status(400).json({ error: 'Invalid location ID' });
+        return;
     }
 
-    res.json(locations);
+    // Define an SQL query to delete the location by ID
+    const query = 'DELETE FROM locations WHERE id = ?';
+
+    db.run(query, [locationId], function (err) {
+        if (err) {
+            console.error('Error deleting location from SQLite database:', err);
+            res.status(500).json({ error: 'Error removing location' });
+        } else {
+            if (this.changes > 0) {
+                res.json({ message: 'Location removed successfully' });
+            } else {
+                res.status(404).json({ error: 'Location not found' });
+            }
+        }
+    });
 });
 
 // Start the server
